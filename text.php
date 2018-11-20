68c09b1b89bd65737c11f2c57b444f29e8f9cdce
Fabian Sonnenberg <fabian.tomoko@gmail.com>
2018-11-20 20:51:06 +0100

    Comments and more
    Added comments, removed unused lines, added toLower Function

--- text.php
+++ text.php
@@ -3,16 +3,23 @@
 require_once("config.php");
 $mysqli = new mysqli($sqlhost, $sqluser, $sqlpass, $sqldb);
 
+$buttonCaption = "String to Lower";
+
 $result = $mysqli->query("SELECT meldung FROM meldungen ORDER BY meldung");
 while($row = $result->fetch_assoc())
 {
 	if (isset($_GET['cut']) && $_GET['cut'] == "yes") { 
+		// Abschneiden der spezfisischen Zusatzinformationen
 		$text = substr($row['meldung'], 0, strpos($row['meldung'], "*"));
 		if (isset($_GET['deeper']) && $_GET['deeper'] == "yes") { 
+			// Entfernen von (H) Haltestellen Informationen
 			if (strpos($text, "(H)"))
 			{
 				$text = substr($text, 0, strpos($text, "(H)"));
 			}
+			/**
+			 * Abschneiden von weiterem überflüssigem Text
+			 */
 			if (strpos($text, "im Bereich"))
 			{
 				$text = substr($text, 0, strpos($text, "im Bereich"));
@@ -26,21 +33,26 @@ while($row = $result->fetch_assoc())
 			$text = str_replace("Die", "", $text);
 			$text = str_replace("Das", "", $text);
 		}
+		
 	} else {
 		$text = $row['meldung'];
 	}
+	if (isset($_POST['tolower']) && $_POST['tolower'] == "String to Lower")
+	{
+		$text = strtolower($text);
+		$buttonCaption = "Normale Schreibweise";
+	}
+	$text = trim($text);
 	$md5 = md5($text);
+	// Prüfen anhand des Hashes ob Text bereits so vorhanden
 	if (!isset($array[$md5]))
 	{
-		$array[$md5] = $text;
+		$array[$md5] = $text; // Text samt Hash speichern
 	}
-	//echo substr($row['meldung'], 0, strpos($row['meldung'], "*"))."<br>";
-	//echo $row['meldung']."<br>";
-
 
 }
 $result->close();
-echo "<a href=\"text.php\">Kompletter Text</a>&nbsp;&nbsp;<a href=\"text.php?cut=yes\">Beschneiden</a>&nbsp;&nbsp;<a href=\"text.php?cut=yes&deeper=yes\">Beschneiden und reduzieren</a><br><br>";
+echo "<a href=\"text.php\">Kompletter Text</a>&nbsp;&nbsp;<a href=\"text.php?cut=yes\">Beschneiden</a>&nbsp;&nbsp;<a href=\"text.php?cut=yes&deeper=yes\">Beschneiden und reduzieren</a><br><br><form method=\"post\"><input type=\"submit\" name=\"tolower\" value=\"".$buttonCaption."\"></form><hr><br>";
 foreach($array as $k => $v)
 {
 	echo $v."<br>";
